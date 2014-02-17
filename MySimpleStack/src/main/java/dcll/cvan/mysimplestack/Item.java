6ff7f43243d6b9f260229c000178b8963d48e5fe
scrouillou <clement.vannier@free.fr>
2014-02-17 13:54:51 +0100

    #3 Implémentation MSS et Item

---
+++ MySimpleStack/src/main/java/dcll/cvan/mysimplestack/Item.java
@@ -0,0 +1,4 @@
+package dcll.cvan.mysimplestack;
+
+public class Item {
+}
---
+++ MySimpleStack/src/main/java/dcll/cvan/mysimplestack/SimpleStack.java
@@ -0,0 +1,35 @@
+package dcll.cvan.mysimplestack;
+
+import java.util.EmptyStackException;
+
+public interface SimpleStack {
+    /**
+     * Tests if this stack is empty
+     */
+    boolean isEmpty();
+
+    /**
+     * Returns the number of items in this stack.
+     */
+    int getSize();
+
+    /**
+     * Pushes an item onto the top of this stack. null item is allowed.
+     */
+    void push(Item item);
+
+    /**
+     * Looks at the object at the top of this stack without removing it from the
+     * stack.
+     */
+    Item peek() throws EmptyStackException;
+
+    /**
+     * Removes the object at the top of this stack and returns that object as
+     * the value of this function.
+     * 
+     * @throws EmptyStackException
+     *             if this stack is empty.
+     */
+    Item pop() throws EmptyStackException;
+}
